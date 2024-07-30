3d4681070e2f6a6e673bfebb63ceef10595aaa76
nilesh2630 <ng.niesh123@gmail.com>
2024-07-31 01:06:09 +0530

    title name change

--- frontend/src/components/Pages/HomePage.js
+++ frontend/src/components/Pages/HomePage.js
@@ -23,7 +23,7 @@ const HomePage = () => {
         borderWidth="1px"
       >
         <Text fontSize="4xl" fontFamily="Work sans" color="black">
-          Talk-A-Tive
+          TalkZone
         </Text>
       </Box>
       <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
