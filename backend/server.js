a8087ac55e3cc855f79fb55effbe53910da9681b
nilesh2630 <ng.niesh123@gmail.com>
2024-07-31 01:32:12 +0530

    frontend url updated

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ dotenv.config()
 connectDB()
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://talkzone-smoky.vercel.app/",
 }));
 app.use('/api/user',userRoutes)
 app.use("/api/chat",chatRoutes)
@@ -26,7 +26,7 @@ const server=app.listen(5000,console.log("Server Started on port 5000"));
 const io=require('socket.io')(server,{
     pingTimeout:60000,
     cors:{
-        origin:"http://localhost:3000"
+        origin:"https://talkzone-smoky.vercel.app/"
         }
 
 })
