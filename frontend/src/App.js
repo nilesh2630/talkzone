5ec6585c7dd82d1529cc2d22467817e7f2820d88
nilesh2630 <ng.niesh123@gmail.com>
2024-07-31 02:24:06 +0530

    nav change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
-
 import './App.css';
-import {Route} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/Pages/HomePage';
 import ChatPage from './components/Pages/ChatPage';
+
 function App() {
   return (
     <div className="App">
-    
-   <Route path="/" component={HomePage} exact />
-   <Route path="/chats" component={ChatPage} />
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/chats" element={<ChatPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
